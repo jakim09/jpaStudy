7d9d55e79efd8ceb6b954b9c3c5aff02479354a0
jakim09 <jakim09@naver.com>
2023-02-26 15:52:41 +0900

    ADD OrderRepository, OrderService (주문 도메인 개발)

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    public final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    // Querydsl: 동적쿼리를 짤 수 있는 효과적인 방법
+//    public List<Order> findAll(OrderSearch orderSearch) {}
+
+}
