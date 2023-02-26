e45672a215f020a9358d923c9e5cdb445d573a15
jakim09 <jakim09@naver.com>
2023-02-26 20:50:10 +0900

    UPDATE ItemController, ItemService (변경감지와 병합 / 변경감지로 변경)

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,17 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+    // 엔티티 변경 시 변경감지 사용하기!
+    // merge의 경우 변경하는 값 외의 값은 NULL로 업데이트 될 가능성 있음 << 사용 X
+    // set보다는 change 등의 메소드를 선언해서 변경하자
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity){
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
